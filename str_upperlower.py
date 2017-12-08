b875ee95762affbc66f330724ec31beb849601a9
sishitam <mail.bl4ckside@gmail.com>
2017-12-08 21:29:51 +0700

    Pembahasan fungsi di method string selesai

---
+++ str_upperlower.py
@@ -0,0 +1,11 @@
+def main():
+    str1 = "I Like Python"
+    str2 = str1.upper()
+    str3 = str1.lower()
+
+    print("str1: ", str1)
+    print("str2: ", str2)
+    print("str3: ", str3)
+
+if __name__ == "__main__":
+    main()
---
+++ str_zfill.py
@@ -0,0 +1,12 @@
+def main():
+    str1 = "9"
+    str2 = "7"
+
+    print("str1: ", str1)
+    print("str2: ", str2)
+
+    print("\nstr1.zfill(2): ", str1.zfill(2))
+    print("str2.zfill(3): ", str2.zfill(3))
+
+if __name__ == "__main__":
+    main()
